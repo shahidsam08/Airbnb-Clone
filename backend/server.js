65222b04221c47d48934dcc3362e3fe58e63d903
Md Shahid Ali <shahidali39374@gmail.com>
2025-12-13 02:54:04 +0530

    changes in the backend file and deploy the backend on render platform

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,8 @@ import express from "express";
 
 const app = express();
 
+const port = process.env.PORT || 8080;
+
 app.get("/" , (req, res) => {
   console.log(req.header)
   res.json({message : "Initial setup chek"})
@@ -11,6 +13,6 @@ app.get("/helpcenter", (req, res) => {
   res.json({message : "Help center page response"})
 })
 
-app.listen(3000, () => {
-  console.log("http://localhost:3000");
+app.listen(port, () => {
+  console.log(`http://localhost:${port}`);
 })
