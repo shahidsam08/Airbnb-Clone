3c174bde978805b29670dba42a7d3e9a4206f5bb
Md Shahid Ali <shahidali39374@gmail.com>
2025-12-28 03:10:32 +0530

    Fix some error

--- frontend/src/components/Searchpage.jsx
+++ frontend/src/components/Searchpage.jsx
@@ -3,11 +3,9 @@ import React from 'react'
 function Searchpage() {
   return (
     <div>
-      <p>this is the home search page</p>
+      <p>this is the search page</p>
     </div>
   )
 }
 
 export default Searchpage
-
-
