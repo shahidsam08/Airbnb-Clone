c465f933157358d64e91d4ecf32be8d0ee40f654
Md Shahid Ali <shahidali39374@gmail.com>
2026-01-01 23:52:59 +0530

    Exprience page where all the data is show which are available in that city where you want to visit.

--- frontend/src/pages/Exprience.jsx
+++ frontend/src/pages/Exprience.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Exprience() {
   return (
     <div className='p-5'>
-      <p>this is hte exprience page</p>
+      <p>this is hte exprience page where data from the database show the exprience in that city where you want to go to visit or travel to that city.</p>
     </div>
   )
 }
