ed7b11515bd02f933bea10a9ca664425aeab4c89
Md Shahid Ali <shahidali39374@gmail.com>
2026-01-14 20:32:36 +0530

    Adding the Messages, profile, trips and other pages which are shown when the user is logged in

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -14,6 +14,7 @@ import { SlLogout } from "react-icons/sl";
 import { Link, NavLink } from "react-router-dom";
 import api from "../api/axios.js";
 import HeaderCommon from "../components/HeaderCommon.jsx";
+import { toast } from "react-toastify";
 
 const reducer = (state, action) => {
   switch (action.type) {
